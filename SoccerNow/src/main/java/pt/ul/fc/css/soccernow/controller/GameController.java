56e89cce9c73ee2e4674e100ac09ee43b072b026
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-04 22:50:22 +0100

    Add some new Swagger annotations

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/controller/GameController.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/controller/GameController.java
@@ -1,9 +1,13 @@
 package pt.ul.fc.css.soccernow.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ul.fc.css.soccernow.service.GameService;
 
-@RestController("/api/games/")
+@Tag(name = "Game", description = "Game operations")
+@RestController
+@RequestMapping("/api/games/")
 public class GameController {
 
   private final GameService gameService;
