d42ebe71e0bbd1ad7a7fb231885828a1701dedf8
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-05 16:33:57 +0100

    Update to abstract all stat related info to the stats entity

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/Stats.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/Stats.java
@@ -1,3 +1,10 @@
 package pt.ul.fc.css.soccernow.domain.entities;
 
-public class Stats {}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stats {
+  private int teamOneGoals = 0;
+  private int teamTwoGoals = 0;
+  private List<Card> givenCards = new ArrayList<>();
+}
--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/game/Game.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/game/Game.java
@@ -1,25 +1,21 @@
 package pt.ul.fc.css.soccernow.domain.entities.game;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import pt.ul.fc.css.soccernow.domain.entities.Card;
 import pt.ul.fc.css.soccernow.domain.entities.GameTeam;
 import pt.ul.fc.css.soccernow.domain.entities.Stats;
 import pt.ul.fc.css.soccernow.domain.entities.user.Referee;
 
 public abstract class Game {
-  private int teamOneGoals = 0;
-  private int teamTwoGoals = 0;
-  private Stats stats;
-  private boolean isOver = false;
-  private String location;
-  private LocalDateTime dateTime;
-  private List<Card> cards = new ArrayList<>();
   public GameTeam teamOne;
   public GameTeam teamTwo;
   public Referee primaryReferee;
   public List<Referee> secondaryReferees;
+
+  private boolean isOver = false;
+  private String location;
+  private LocalDateTime dateTime;
+  private Stats stats;
 
   public Game(String location, LocalDateTime dateTime) {
     this.location = location;
@@ -27,24 +23,6 @@ public abstract class Game {
   }
 
   public Game() {}
-
-  public int getTeamOneGoals() {
-    return teamOneGoals;
-  }
-
-  public Game setTeamOneGoals(int teamOneGoals) {
-    this.teamOneGoals = teamOneGoals;
-    return this;
-  }
-
-  public int getTeamTwoGoals() {
-    return teamTwoGoals;
-  }
-
-  public Game setTeamTwoGoals(int teamTwoGoals) {
-    this.teamTwoGoals = teamTwoGoals;
-    return this;
-  }
 
   public Stats getStats() {
     return stats;
@@ -79,15 +57,6 @@ public abstract class Game {
 
   public Game setDateTime(LocalDateTime dateTime) {
     this.dateTime = dateTime;
-    return this;
-  }
-
-  public List<Card> getCards() {
-    return cards;
-  }
-
-  public Game setCards(List<Card> cards) {
-    this.cards = cards;
     return this;
   }
 }
