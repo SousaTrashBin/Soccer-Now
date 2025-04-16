59b10689c0e8b2a09cca739648aedf4eac9883d6
sofia <sofia.reia.pt@gmail.com>
2025-04-16 22:18:24 +0100

    Add basic feature skeleton for PlayerService

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/PlayerService.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/PlayerService.java
@@ -1,3 +1,14 @@
 package pt.ul.fc.css.soccernow.service;
 
-public interface PlayerService {}
+import pt.ul.fc.css.soccernow.domain.entities.user.Player;
+
+import java.util.UUID;
+
+public interface PlayerService {
+    boolean exists(UUID playerId);
+    Player add(Player player);
+    Player getById(UUID playerId);
+    Player getByName(String name);
+    Player update(Player player);
+    Player remove(UUID playerId);
+}
--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/PlayerServiceImpl.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/PlayerServiceImpl.java
@@ -1,7 +1,51 @@
 package pt.ul.fc.css.soccernow.service.impl;
 
 import org.springframework.stereotype.Service;
+import pt.ul.fc.css.soccernow.domain.entities.user.Player;
+import pt.ul.fc.css.soccernow.exception.ResourceAlreadyExistsException;
+import pt.ul.fc.css.soccernow.repository.PlayerRepository;
 import pt.ul.fc.css.soccernow.service.PlayerService;
 
+import java.util.UUID;
+
 @Service
-public class PlayerServiceImpl implements PlayerService {}
+public class PlayerServiceImpl implements PlayerService {
+    private final PlayerRepository playerRepository;
+
+    public PlayerServiceImpl(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @Override
+    public boolean exists(UUID playerId) {
+        return playerRepository.existsById(playerId);
+    }
+
+    @Override
+    public Player add(Player player) {
+        Player newPlayer = new Player();
+        newPlayer.setName(player.getName());
+        newPlayer.setPreferredPosition(player.getPreferredPosition());
+        return playerRepository.save(newPlayer); //201
+    }
+
+    @Override
+    public Player getById(UUID playerId) {
+        return null;
+    }
+
+    @Override
+    public Player getByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Player update(Player player) {
+        return null;
+    }
+
+    @Override
+    public Player remove(UUID playerId) {
+        return null;
+    }
+}
