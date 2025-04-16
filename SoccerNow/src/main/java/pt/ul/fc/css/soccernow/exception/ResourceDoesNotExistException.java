cf623000cc384ed8b37b8aa8107a9d228586731a
sofia <sofia.reia.pt@gmail.com>
2025-04-16 23:37:16 +0100

    Add an exception to represent if a resource does not exist

---
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/exception/ResourceDoesNotExistException.java
@@ -0,0 +1,13 @@
+package pt.ul.fc.css.soccernow.exception;
+
+public class ResourceDoesNotExistException extends RuntimeException {
+    public ResourceDoesNotExistException(String message) {
+        super(message);
+    }
+    public ResourceDoesNotExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public ResourceDoesNotExistException(String resourceName, String fieldName, Object fieldValue) {
+        super("Resource %s with %s = %s does not exist".formatted(resourceName, fieldName, fieldValue));
+    }
+}
