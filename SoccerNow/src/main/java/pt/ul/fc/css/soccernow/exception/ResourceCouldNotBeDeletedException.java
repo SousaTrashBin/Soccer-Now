2987ca0e829cd7870c914e443c236ac1f8c39772
sofia <sofia.reia.pt@gmail.com>
2025-04-16 23:37:03 +0100

    Add an exception to represent if a resource could not be deleted

---
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/exception/ResourceCouldNotBeDeletedException.java
@@ -0,0 +1,11 @@
+package pt.ul.fc.css.soccernow.exception;
+
+public class ResourceCouldNotBeDeletedException extends RuntimeException {
+    public ResourceCouldNotBeDeletedException(String message) {
+        super(message);
+    }
+    public ResourceCouldNotBeDeletedException(String message, Throwable cause) {}
+    public ResourceCouldNotBeDeletedException(String resourceName, String fieldName, Object fieldValue) {
+        super("Resource %s with %s = %s could not be deleted".formatted(resourceName, fieldName, fieldValue));
+    }
+}
