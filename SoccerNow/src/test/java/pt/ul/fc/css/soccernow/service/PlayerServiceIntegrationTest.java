e3b4c9446a8693d1f05ffeda0f23ab5b05d476c9
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-16 22:33:11 +0100

    Add simple integration test for PlayerService

---
+++ SoccerNow/src/test/java/pt/ul/fc/css/soccernow/service/PlayerServiceIntegrationTest.java
@@ -0,0 +1,32 @@
+package pt.ul.fc.css.soccernow.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static pt.ul.fc.css.soccernow.PlayerTestDataUtil.getPlayers;
+
+import java.util.Objects;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pt.ul.fc.css.soccernow.domain.entities.user.Player;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class PlayerServiceIntegrationTest {
+  @Autowired private PlayerService underTest;
+
+  @Test
+  public void addIfValidPlayerCanBeAdded() {
+    Player player = getPlayers().get(0);
+    Player addedPlayer = underTest.add(player);
+
+    assert addedPlayer.getId() != null
+        && addedPlayer.getName().equals(player.getName())
+        && Objects.equals(addedPlayer.getPreferredPosition(), player.getPreferredPosition());
+
+    assert underTest.exists(addedPlayer.getId());
+  }
+}
