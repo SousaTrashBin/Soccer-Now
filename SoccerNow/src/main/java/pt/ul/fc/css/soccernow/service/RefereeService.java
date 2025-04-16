f8b4b7329688f61907be8a880ff6cb53e8a6760f
sofia <sofia.reia.pt@gmail.com>
2025-04-16 23:39:07 +0100

    Add basic feature skeleton for RefereeService

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/RefereeService.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/RefereeService.java
@@ -1,3 +1,20 @@
 package pt.ul.fc.css.soccernow.service;
 
-public interface RefereeService {}
+import org.springframework.transaction.annotation.Transactional;
+import pt.ul.fc.css.soccernow.domain.entities.user.Referee;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface RefereeService {
+    boolean existsById(UUID refereeId);
+    boolean existsByName(String refereeName);
+    @Transactional
+    Referee add(Referee referee);
+    Referee getById(UUID refereeId);
+    List<Referee> getByName(String refereeName);
+    @Transactional
+    Referee update(Referee referee);
+    @Transactional
+    void remove(UUID refereeId);
+}
--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/RefereeServiceImpl.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/RefereeServiceImpl.java
@@ -1,7 +1,45 @@
 package pt.ul.fc.css.soccernow.service.impl;
 
 import org.springframework.stereotype.Service;
+import pt.ul.fc.css.soccernow.domain.entities.user.Referee;
 import pt.ul.fc.css.soccernow.service.RefereeService;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
-public class RefereeServiceImpl implements RefereeService {}
+public class RefereeServiceImpl implements RefereeService {
+    @Override
+    public boolean existsById(UUID refereeId) {
+        return false;
+    }
+
+    @Override
+    public boolean existsByName(String refereeName) {
+        return false;
+    }
+
+    @Override
+    public Referee add(Referee referee) {
+        return null;
+    }
+
+    @Override
+    public Referee getById(UUID refereeId) {
+        return null;
+    }
+
+    @Override
+    public List<Referee> getByName(String refereeName) {
+        return List.of();
+    }
+
+    @Override
+    public Referee update(Referee referee) {
+        return null;
+    }
+
+    @Override
+    public void remove(UUID refereeId) {
+    }
+}
