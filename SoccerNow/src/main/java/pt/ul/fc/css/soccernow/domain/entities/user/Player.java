ec9d40f3d010a0fdc0fbdd2ff8831fe58ea51b0d
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-17 19:45:19 +0100

    Add addTeam and removeTeam functions to abstract that from higher levels of abstraction

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/user/Player.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/user/Player.java
@@ -1,60 +1,71 @@
 package pt.ul.fc.css.soccernow.domain.entities.user;
 
 import jakarta.persistence.*;
-import java.util.*;
 import pt.ul.fc.css.soccernow.domain.entities.Team;
 import pt.ul.fc.css.soccernow.domain.entities.game.PlayerGameStats;
 import pt.ul.fc.css.soccernow.util.FutsalPositionEnum;
 
+import java.util.*;
+
 @Entity
 public class Player extends User {
-  @Enumerated(EnumType.STRING)
-  @Column(name = "preferred_position")
-  private FutsalPositionEnum preferredPosition;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "preferred_position")
+    private FutsalPositionEnum preferredPosition;
 
-  @ManyToMany(mappedBy = "players")
-  @OrderBy("name")
-  private Set<Team> teams = new LinkedHashSet<>();
+    @ManyToMany(mappedBy = "players")
+    @OrderBy("name")
+    private Set<Team> teams = new LinkedHashSet<>();
 
-  @OneToMany(orphanRemoval = true)
-  private List<PlayerGameStats> playerGameStats = new ArrayList<>();
+    @OneToMany(orphanRemoval = true)
+    private List<PlayerGameStats> playerGameStats = new ArrayList<>();
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Player player = (Player) o;
-    return Objects.equals(getId(), player.getId())
-        && Objects.equals(getName(), player.getName())
-        && getPreferredPosition() == player.getPreferredPosition();
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Player player = (Player) o;
+        return Objects.equals(getId(), player.getId());
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(getId(), getName(), getPreferredPosition());
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 
-  public List<PlayerGameStats> getPlayerGameStats() {
-    return playerGameStats;
-  }
+    public List<PlayerGameStats> getPlayerGameStats() {
+        return playerGameStats;
+    }
 
-  public void setPlayerGameStats(List<PlayerGameStats> playerGameStats) {
-    this.playerGameStats = playerGameStats;
-  }
+    public void setPlayerGameStats(List<PlayerGameStats> playerGameStats) {
+        this.playerGameStats = playerGameStats;
+    }
 
-  public Set<Team> getTeams() {
-    return teams;
-  }
+    public Set<Team> getTeams() {
+        return teams;
+    }
 
-  public void setTeams(Set<Team> teams) {
-    this.teams = new LinkedHashSet<>(teams);
-  }
+    public void setTeams(Set<Team> teams) {
+        this.teams = new LinkedHashSet<>(teams);
+    }
 
-  public FutsalPositionEnum getPreferredPosition() {
-    return preferredPosition;
-  }
+    public FutsalPositionEnum getPreferredPosition() {
+        return preferredPosition;
+    }
 
-  public void setPreferredPosition(FutsalPositionEnum preferredPosition) {
-    this.preferredPosition = preferredPosition;
-  }
+    public void setPreferredPosition(FutsalPositionEnum preferredPosition) {
+        this.preferredPosition = preferredPosition;
+    }
+
+    public void addTeam(Team team) {
+        teams.add(team);
+    }
+
+    public void removeTeam(Team team) {
+        teams.remove(team);
+    }
 }
