65bc8e978090f843f35d1d0752fdbeb547f42a34
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-18 03:58:03 +0100

    Add some helper functions in order to manage game business logic

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/GameService.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/GameService.java
@@ -1,6 +1,9 @@
 package pt.ul.fc.css.soccernow.service;
 
+import pt.ul.fc.css.soccernow.domain.entities.Team;
 import pt.ul.fc.css.soccernow.domain.entities.game.Game;
+import pt.ul.fc.css.soccernow.domain.entities.user.Player;
 
 public interface GameService extends CrudService<Game> {
+    boolean hasPendingGame(Player player, Team team);
 }
