1adf918ed95e679fce5b446ae60b7549bf11c8bb
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-18 03:53:29 +0100

    Add referee service implementation

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/RefereeServiceImpl.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/RefereeServiceImpl.java
@@ -2,6 +2,9 @@ package pt.ul.fc.css.soccernow.service.impl;
 
 import org.springframework.stereotype.Service;
 import pt.ul.fc.css.soccernow.domain.entities.user.Referee;
+import pt.ul.fc.css.soccernow.exception.ResourceCouldNotBeDeletedException;
+import pt.ul.fc.css.soccernow.exception.ResourceDoesNotExistException;
+import pt.ul.fc.css.soccernow.repository.RefereeRepository;
 import pt.ul.fc.css.soccernow.service.RefereeService;
 
 import java.util.List;
@@ -9,32 +12,60 @@ import java.util.UUID;
 
 @Service
 public class RefereeServiceImpl implements RefereeService {
+
+    private final RefereeRepository refereeRepository;
+
+    public RefereeServiceImpl(RefereeRepository refereeRepository) {
+        this.refereeRepository = refereeRepository;
+    }
+
     @Override
     public Referee add(Referee entity) {
-        return null;
+        Referee newReferee = new Referee();
+        newReferee.setHasCertificate(entity.getHasCertificate());
+        newReferee.setName(entity.getName());
+        return refereeRepository.save(newReferee);
     }
 
     @Override
     public Referee update(Referee entity) {
-        return null;
+        Referee referee = findNotDeletedById(entity.getId());
+        if (referee.getName() != null) {
+            referee.setName(entity.getName());
+        }
+        if (entity.getHasCertificate() != null) {
+            referee.setHasCertificate(entity.getHasCertificate());
+        }
+        return refereeRepository.save(referee);
     }
 
     @Override
     public void softDelete(UUID entityId) {
+        Referee referee = findNotDeletedById(entityId);
+        if (referee.hasAnyPendingGames()) {
+            throw new ResourceCouldNotBeDeletedException("Referee %s has pending games".formatted(referee.getName()));
+        }
+        referee.delete();
+        refereeRepository.save(referee);
     }
 
     @Override
     public List<Referee> findAllNotDeleted() {
-        return List.of();
+        return refereeRepository.findAllNotDeleted();
     }
 
     @Override
     public Referee findById(UUID entityId) {
-        return null;
+        return refereeRepository.findById(entityId)
+                                .orElseThrow(() -> new ResourceDoesNotExistException("Referee", "id", entityId));
     }
 
     @Override
     public Referee findNotDeletedById(UUID entityId) {
-        return null;
+        Referee referee = findById(entityId);
+        if (referee.isDeleted()) {
+            throw new ResourceDoesNotExistException("Referee", "id", entityId);
+        }
+        return referee;
     }
 }
