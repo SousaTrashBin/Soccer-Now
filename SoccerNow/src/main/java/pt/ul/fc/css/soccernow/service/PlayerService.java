a52d69bee9b4a75e0503cfff04e6d04e2c3e70ff
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-18 03:57:26 +0100

    Fix since team is the owner of the relationship, there's no need to manually update both sides of the relationship Simplified a bit the logic of the update method

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/PlayerService.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/PlayerService.java
@@ -1,13 +1,7 @@
 package pt.ul.fc.css.soccernow.service;
 
-import org.springframework.transaction.annotation.Transactional;
-import pt.ul.fc.css.soccernow.domain.entities.Team;
 import pt.ul.fc.css.soccernow.domain.entities.user.Player;
 
 public interface PlayerService extends CrudService<Player> {
-    @Transactional
-    void removeTeamFromPlayer(Team team, Player savedPlayer);
 
-    @Transactional
-    void addTeamToPlayer(Team team, Player savedPlayer);
 }
