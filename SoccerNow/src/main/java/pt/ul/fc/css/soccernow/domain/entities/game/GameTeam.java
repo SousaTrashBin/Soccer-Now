c9843ef2f91f8db3a3ef04926eb059f7d3f5bd3f
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-18 03:57:44 +0100

    Add some helper functions in order to manage game business logic

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/game/GameTeam.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/game/GameTeam.java
@@ -3,6 +3,8 @@ package pt.ul.fc.css.soccernow.domain.entities.game;
 import jakarta.persistence.*;
 import pt.ul.fc.css.soccernow.domain.entities.Team;
 import pt.ul.fc.css.soccernow.domain.entities.tournament.GamePlayer;
+import pt.ul.fc.css.soccernow.domain.entities.user.Player;
+import pt.ul.fc.css.soccernow.util.FutsalPositionEnum;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,12 +13,13 @@ import java.util.UUID;
 @Entity
 @Table(name = "game_teams")
 public class GameTeam {
+    public static final int FUTSAL_TEAM_SIZE = 5;
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false)
     private UUID id;
 
-    @OneToMany(orphanRemoval = true)
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
     @JoinColumn(name = "game_team_id")
     private Set<GamePlayer> gamePlayers = new HashSet<>();
 
@@ -58,5 +61,22 @@ public class GameTeam {
 
     public void setId(UUID id) {
         this.id = id;
+    }
+
+    public boolean hasPlayer(Player player) {
+        return getGamePlayers().stream()
+                               .map(GamePlayer::getPlayer)
+                               .anyMatch(player::equals);
+    }
+
+    public boolean hasExactlyOneGoalKeeper() {
+        return getGamePlayers().stream()
+                               .map(GamePlayer::getPlayedInPosition)
+                               .map(futsalPosition -> futsalPosition.equals(FutsalPositionEnum.GOALIE))
+                               .count() == 1;
+    }
+
+    public boolean hasTheRightSize() {
+        return getGamePlayers().size() == FUTSAL_TEAM_SIZE;
     }
 }
