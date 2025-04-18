7fbb0486a7811da3cf05467654d7c0918d4538b4
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-18 03:52:38 +0100

    Add annotations that represent the status code of the respective exception

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/exception/ResourceDoesNotExistException.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/exception/ResourceDoesNotExistException.java
@@ -1,5 +1,9 @@
 package pt.ul.fc.css.soccernow.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceDoesNotExistException extends RuntimeException {
     public ResourceDoesNotExistException(String message) {
         super(message);
