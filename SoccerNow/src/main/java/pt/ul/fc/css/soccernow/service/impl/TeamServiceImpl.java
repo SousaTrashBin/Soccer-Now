03b2c2a076b25d685e3a3290d1a1a8e7353d79af
Renan Silva fc59802 <fc59802@alunos.fc.ul.pt>
2025-04-19 19:16:06 +0100

    Add some implementation for a few methods in TeamServiceImpl

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/TeamServiceImpl.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/TeamServiceImpl.java
@@ -1,23 +1,59 @@
 package pt.ul.fc.css.soccernow.service.impl;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import pt.ul.fc.css.soccernow.domain.entities.Team;
 import pt.ul.fc.css.soccernow.domain.entities.user.Player;
+import pt.ul.fc.css.soccernow.domain.entities.user.Referee;
+import pt.ul.fc.css.soccernow.exception.BadRequestException;
+import pt.ul.fc.css.soccernow.exception.ResourceDoesNotExistException;
+import pt.ul.fc.css.soccernow.repository.PlayerRepository;
+import pt.ul.fc.css.soccernow.repository.TeamRepository;
+import pt.ul.fc.css.soccernow.service.PlayerService;
 import pt.ul.fc.css.soccernow.service.TeamService;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class TeamServiceImpl implements TeamService {
+    private final TeamRepository teamRepository;
+    private final PlayerRepository playerRepository;
+    private final PlayerService playerService;
+
+    public TeamServiceImpl(
+        TeamRepository teamRepository,
+        PlayerRepository playerRepository,
+        @Lazy PlayerService playerService
+    ) {
+        this.teamRepository = teamRepository;
+        this.playerRepository = playerRepository;
+        this.playerService = playerService;
+    }
+
     @Override
     public void removePlayerFromTeam(Player savedPlayer, Team team) {
-
+        if (team.hasPlayer(savedPlayer)) {
+            team.getPlayers().remove(savedPlayer);
+            teamRepository.save(team);
+            playerRepository.save(savedPlayer);
+        } else {
+            throw new BadRequestException(savedPlayer.getName() + " is not on team " + team.getName());
+        }
     }
 
     @Override
     public void addPlayerToTeam(Player savedPlayer, Team team) {
+        boolean validTransaction = !savedPlayer.hasTeam(team) && !team.hasPlayer(savedPlayer);
 
+        if (validTransaction) {
+            team.addPlayer(savedPlayer);
+            savedPlayer.addTeam(team);
+            teamRepository.save(team);
+
+        } else {
+            throw new BadRequestException(savedPlayer.getName() + " is already on team " + team.getName());
+        }
     }
 
     @Override
@@ -27,7 +63,21 @@ public class TeamServiceImpl implements TeamService {
 
     @Override
     public Team add(Team entity) {
-        return null;
+        Team newTeam = new Team();
+        newTeam.setName(entity.getName());
+
+        List<Player> newTeamPlayers = entity.getPlayers()
+                                            .stream()
+                                            .map(p -> playerService.findNotDeletedById(p.getId()))
+                                            .toList();
+
+        newTeam.setPlayers(newTeamPlayers);
+        Team savedTeam = teamRepository.save(newTeam);
+
+        for (Player player : savedTeam.getPlayers())
+            playerService.addTeamToPlayer(savedTeam, player);
+
+        return savedTeam;
     }
 
     @Override
@@ -46,11 +96,18 @@ public class TeamServiceImpl implements TeamService {
 
     @Override
     public Team findById(UUID entityId) {
-        return null;
+        return teamRepository.findById(entityId)
+                             .orElseThrow(() -> new ResourceDoesNotExistException("Team", "id", entityId));
     }
 
     @Override
     public Team findNotDeletedById(UUID entityId) {
-        return null;
+        Team team = findById(entityId);
+
+        if (team.isDeleted()) {
+            throw new ResourceDoesNotExistException("Team", "id", entityId);
+        }
+
+        return team;
     }
 }
