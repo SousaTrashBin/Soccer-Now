71bfb24b589c69d03797824e7c9959a789dc9d0c
Renan Silva fc59802 <fc59802@alunos.fc.ul.pt>
2025-04-19 19:13:11 +0100

    Add method signature (addTeamToPlayer) to PlayerService

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/PlayerService.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/PlayerService.java
@@ -1,7 +1,8 @@
 package pt.ul.fc.css.soccernow.service;
 
+import pt.ul.fc.css.soccernow.domain.entities.Team;
 import pt.ul.fc.css.soccernow.domain.entities.user.Player;
 
 public interface PlayerService extends CrudService<Player> {
-
+    void addTeamToPlayer(Team savedTeam, Player player);
 }
