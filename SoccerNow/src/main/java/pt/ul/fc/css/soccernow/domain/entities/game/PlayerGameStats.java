90219b6ba16c15f299ca8deb344ca5387d09c094
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-22 00:20:52 +0100

    Fix to make sure player game stats persists it's changes

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/game/PlayerGameStats.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/game/PlayerGameStats.java
@@ -18,15 +18,16 @@ public class PlayerGameStats {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "given_card")
-    private CardEnum givenCard = CardEnum.RED;
+    private CardEnum givenCard = CardEnum.NONE;
 
     @Column(name = "scored_goals", nullable = false)
     private Integer scoredGoals = 0;
 
-    @ManyToOne(optional = false)
+    @ManyToOne(optional = false, cascade = CascadeType.ALL)
     @JoinColumn(name = "game_stats_id", nullable = false)
     private GameStats gameStats;
-    @ManyToOne
+
+    @ManyToOne(optional = false, cascade = CascadeType.ALL)
     @JoinColumn(name = "player_id")
     private Player player;
 
@@ -62,39 +63,6 @@ public class PlayerGameStats {
         this.id = id;
     }
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null) {
-            return false;
-        }
-        Class<?> oEffectiveClass =
-                o instanceof HibernateProxy
-                        ? ((HibernateProxy) o).getHibernateLazyInitializer()
-                        .getPersistentClass()
-                        : o.getClass();
-        Class<?> thisEffectiveClass =
-                this instanceof HibernateProxy
-                        ? ((HibernateProxy) this).getHibernateLazyInitializer()
-                        .getPersistentClass()
-                        : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) {
-            return false;
-        }
-        PlayerGameStats that = (PlayerGameStats) o;
-        return getId() != null && Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy
-                ? ((HibernateProxy) this).getHibernateLazyInitializer()
-                .getPersistentClass()
-                .hashCode()
-                : getClass().hashCode();
-    }
 
     public Player getPlayer() {
         return player;
@@ -102,5 +70,31 @@ public class PlayerGameStats {
 
     public void setPlayer(Player player) {
         this.player = player;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        PlayerGameStats that = (PlayerGameStats) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "givenCard = " + givenCard + ", " +
+                "scoredGoals = " + scoredGoals + ", " +
+                "gameStats = " + gameStats + ", " +
+                "player = " + player + ")";
     }
 }
