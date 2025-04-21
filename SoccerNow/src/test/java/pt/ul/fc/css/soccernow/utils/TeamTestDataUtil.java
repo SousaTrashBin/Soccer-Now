bf1dbc2b89eb64921fef6ff75e6eeff0ac7e19d3
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-21 18:03:09 +0100

    Add some simple integration tests

--- SoccerNow/src/test/java/pt/ul/fc/css/soccernow/utils/TeamTestDataUtil.java
+++ SoccerNow/src/test/java/pt/ul/fc/css/soccernow/utils/TeamTestDataUtil.java
@@ -1,4 +1,47 @@
 package pt.ul.fc.css.soccernow.utils;
 
+import pt.ul.fc.css.soccernow.domain.entities.Team;
+import pt.ul.fc.css.soccernow.domain.entities.user.Player;
+
+import java.util.List;
+import java.util.Set;
+
+import static pt.ul.fc.css.soccernow.utils.UserTestDataUtil.RANDOM;
+
 public class TeamTestDataUtil {
+    private static final List<String> RANDOM_TEAM_NAMES = List.of(
+            "Sporting CP",
+            "SL Benfica",
+            "FC Porto",
+            "SC Braga",
+            "AD Fundão",
+            "Leões Porto Salvo",
+            "Eléctrico FC",
+            "Viseu 2001",
+            "Modicus Sandim",
+            "Ferreira do Zêzere",
+            "Amarense",
+            "Os Belenenses",
+            "Rio Ave",
+            "Boavista FC",
+            "Académica Coimbra",
+            "Vitória SC",
+            "Marítimo",
+            "Fabril Barreiro",
+            "Pinhalnovense",
+            "Burinhosa"
+    );
+
+    public static Team createRandomTeam(Set<Player> players) {
+        Team team = new Team();
+        team.setName(getRandomClubName());
+        team.setPlayers(players);
+        return team;
+    }
+
+    public static String getRandomClubName() {
+        int randomIndex = RANDOM.nextInt(RANDOM_TEAM_NAMES.size());
+        return RANDOM_TEAM_NAMES.get(randomIndex);
+    }
+
 }
