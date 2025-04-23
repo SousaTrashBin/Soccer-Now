a8e149b2292d2468f4934899e92b1cbe832643d9
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-04-24 00:17:30 +0100

    Add Player Controller post method as well as a respective test that succeeds

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/controller/PlayerController.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/controller/PlayerController.java
@@ -2,12 +2,18 @@ package pt.ul.fc.css.soccernow.controller;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import pt.ul.fc.css.soccernow.domain.dto.user.reference.PlayerDto;
+import pt.ul.fc.css.soccernow.domain.dto.user.PlayerDTO;
+import pt.ul.fc.css.soccernow.domain.entities.user.Player;
+import pt.ul.fc.css.soccernow.mapper.PlayerMapper;
 import pt.ul.fc.css.soccernow.service.PlayerService;
 
 import java.util.List;
+import java.util.UUID;
 
 @Tag(name = "Player", description = "Player operations")
 @RestController
@@ -15,42 +21,48 @@ import java.util.List;
 public class PlayerController {
 
     private final PlayerService playerService;
+    private final PlayerMapper playerMapper;
 
-    public PlayerController(PlayerService playerService) {
+    public PlayerController(PlayerService playerService, PlayerMapper playerMapper) {
         this.playerService = playerService;
+        this.playerMapper = playerMapper;
     }
 
     @PostMapping
     @ApiOperation(value = "Register a player", notes = "Returns the player registered")
-    public ResponseEntity<PlayerDto> registerPlayer(@RequestBody PlayerDto player) {
-        // TODO
-        return null;
+    public ResponseEntity<PlayerDTO> registerPlayer(@RequestBody @Validated @NotNull PlayerDTO playerDTO) {
+        if (playerDTO.getName() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Player player = playerMapper.toEntity(playerDTO);
+        Player savedPlayer = playerService.add(player);
+        return ResponseEntity.status(HttpStatus.CREATED).body(playerMapper.toDTO(savedPlayer));
     }
 
     @GetMapping("/{playerId}")
     @ApiOperation(value = "Get player by ID", notes = "Returns a player by its ID")
-    public ResponseEntity<PlayerDto> getPlayerById(@PathVariable("playerId") long playerId) {
+    public ResponseEntity<PlayerDTO> getPlayerById(@PathVariable("playerId") UUID playerId) {
         // TODO
         return null;
     }
 
     @GetMapping
     @ApiOperation(value = "Get all players", notes = "Returns a list of all players")
-    public ResponseEntity<List<PlayerDto>> getAllPlayers() {
+    public ResponseEntity<List<PlayerDTO>> getAllPlayers() {
         // TODO
         return null;
     }
 
     @DeleteMapping("/{playerId}")
     @ApiOperation(value = "Delete a player with given ID", notes = "Returns the deleted player")
-    public ResponseEntity<PlayerDto> deletePlayerById(@PathVariable("playerId") long playerId) {
+    public ResponseEntity<PlayerDTO> deletePlayerById(@PathVariable("playerId") UUID playerId) {
         // TODO
         return null;
     }
 
     @PutMapping("/{playerId}")
     @ApiOperation(value = "Update a player with given ID", notes = "Returns the updated player")
-    public ResponseEntity<PlayerDto> updatePlayerById(@PathVariable("playerId") long playerId) {
+    public ResponseEntity<PlayerDTO> updatePlayerById(@PathVariable("playerId") UUID playerId) {
         // TODO
         return null;
     }
---
+++ SoccerNow/src/test/java/pt/ul/fc/css/soccernow/controller/PlayerControllerIntegrationTest.java
@@ -0,0 +1,53 @@
+package pt.ul.fc.css.soccernow.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pt.ul.fc.css.soccernow.domain.dto.user.PlayerDTO;
+import pt.ul.fc.css.soccernow.util.FutsalPositionEnum;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class PlayerControllerIntegrationTest {
+
+    private final MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public PlayerControllerIntegrationTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void testIfValidPlayerCanBeRegistered() throws Exception {
+        PlayerDTO playerDTO = new PlayerDTO()
+                .setName("TEST")
+                .setPreferredPosition(FutsalPositionEnum.GOALIE);
+        String playerDTOJSON = objectMapper.writeValueAsString(playerDTO);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/players/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(playerDTOJSON)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.name").value("TEST")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.preferredPosition").value("GOALIE")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.id").exists()
+        );
+    }
+}
