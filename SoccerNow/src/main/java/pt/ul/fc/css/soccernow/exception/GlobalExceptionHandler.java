fa661cc8f9da18978e288e37e5e5f49cefa2f19d
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-05-03 00:19:42 +0100

    Update, now exceptions thrown by spring validation get handled properly

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/exception/GlobalExceptionHandler.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/exception/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package pt.ul.fc.css.soccernow.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -20,5 +23,18 @@ public class GlobalExceptionHandler {
                 "message", ex.getMessage()
         );
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
