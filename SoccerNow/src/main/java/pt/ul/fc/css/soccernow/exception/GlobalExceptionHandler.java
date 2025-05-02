d5cc90452a74d0f590068a6e94ec2103f7b9a8e7
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-05-02 10:59:39 +0100

    Add global exception handler

---
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+
+package pt.ul.fc.css.soccernow.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<Object> handleBadRequestException(BadRequestException ex) {
+        Map<String, Object> body = Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", HttpStatus.BAD_REQUEST.value(),
+                "error", "Bad Request",
+                "message", ex.getMessage()
+        );
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
