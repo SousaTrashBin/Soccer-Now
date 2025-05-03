7b9f8b9ee391c11cebdf242289bf9d5932efe7e0
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-05-03 15:55:05 +0100

    Fix, no need for the team service

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/PlayerServiceImpl.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/PlayerServiceImpl.java
@@ -1,27 +1,21 @@
 package pt.ul.fc.css.soccernow.service.impl;
 
 import org.springframework.stereotype.Service;
-import pt.ul.fc.css.soccernow.domain.entities.Team;
 import pt.ul.fc.css.soccernow.domain.entities.user.Player;
 import pt.ul.fc.css.soccernow.exception.ResourceCouldNotBeDeletedException;
 import pt.ul.fc.css.soccernow.exception.ResourceDoesNotExistException;
 import pt.ul.fc.css.soccernow.repository.PlayerRepository;
 import pt.ul.fc.css.soccernow.service.PlayerService;
-import pt.ul.fc.css.soccernow.service.TeamService;
 
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
     private final PlayerRepository playerRepository;
-    private final TeamService teamService;
 
-    public PlayerServiceImpl(PlayerRepository playerRepository, TeamService teamService) {
+    public PlayerServiceImpl(PlayerRepository playerRepository) {
         this.playerRepository = playerRepository;
-        this.teamService = teamService;
     }
 
     @Override
@@ -29,7 +23,7 @@ public class PlayerServiceImpl implements PlayerService {
         Player newPlayer = new Player();
         newPlayer.setName(player.getName());
         newPlayer.setPreferredPosition(player.getPreferredPosition());
-        return playerRepository.save(newPlayer); //201
+        return playerRepository.save(newPlayer);
     }
 
     @Override
@@ -55,25 +49,6 @@ public class PlayerServiceImpl implements PlayerService {
     @Override
     public Player update(Player updatedPlayer) {
         Player existingPlayer = findNotDeletedById(updatedPlayer.getId());
-
-        if (updatedPlayer.getTeams() != null) {
-            Set<Team> newTeams = updatedPlayer.getTeams();
-            Set<Team> currentTeams = existingPlayer.getTeams();
-
-            Set<Team> teamsToAdd = newTeams.stream()
-                    .filter(team -> !currentTeams.contains(team))
-                    .collect(Collectors.toSet());
-            for (Team teamToAdd : teamsToAdd) {
-                teamService.addPlayerToTeam(existingPlayer, teamToAdd);
-            }
-
-            Set<Team> teamsToRemove = currentTeams.stream()
-                    .filter(team -> !newTeams.contains(team))
-                    .collect(Collectors.toSet());
-            for (Team teamToRemove : teamsToRemove) {
-                teamService.removePlayerFromTeam(existingPlayer, teamToRemove);
-            }
-        }
 
         if (updatedPlayer.getName() != null) {
             existingPlayer.setName(updatedPlayer.getName());
