5f0a998dd3817bc8ec81386b849032f417130d51
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-05-03 15:55:44 +0100

    Add version to the soft delete entity (maybe the name is not the best but couldn't find a better one) such that every main entity now has a version to treat concurrency problems

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/util/SoftDeleteEntity.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/util/SoftDeleteEntity.java
@@ -2,13 +2,25 @@ package pt.ul.fc.css.soccernow.util;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 public class SoftDeleteEntity {
+    @Version
+    @Column(name = "version")
+    private Integer version;
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
 
     public LocalDateTime getDeletedAt() {
         return deletedAt;
