4e3543e64d0325ab067b59284f1318326dea3432
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-05-27 16:50:44 +0100

    Add new validator (that works with the DTO annotations)

---
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/SoccerNowApp.java
@@ -0,0 +1,44 @@
+package com.soccernow.ui.soccernowui;
+
+import jakarta.validation.Validation;
+import jakarta.validation.ValidatorFactory;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
+
+import java.io.IOException;
+
+public class SoccerNowApp extends Application {
+    private static ValidatorFactory validatorFactory;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        FXMLLoader fxmlLoader = new FXMLLoader(SoccerNowApp.class.getResource("fxml/register-player.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 400);
+
+        scene.getRoot().setStyle("-fx-background-color: white;");
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+
+        stage.setTitle("Soccer Now");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        if (validatorFactory != null) {
+            validatorFactory.close();
+        }
+    }
+
+    public static ValidatorFactory getValidatorFactory() {
+        return validatorFactory;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
