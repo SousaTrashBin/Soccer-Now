0f1dd6433e08e34b365abcdb500bcc0575d7e55d
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-05-27 16:50:19 +0100

    Update to export/open some classes that are needed

--- SoccerNowUI/src/main/java/module-info.java
+++ SoccerNowUI/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.soccernow.ui.soccernowui {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.swing;
+    requires java.desktop;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -9,7 +11,21 @@ module com.soccernow.ui.soccernowui {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires org.jetbrains.annotations;
+    requires jakarta.validation;
+    requires org.hibernate.validator;
+    requires okhttp3;
 
     opens com.soccernow.ui.soccernowui to javafx.fxml;
+
+    opens com.soccernow.ui.soccernowui.dto.user to org.hibernate.validator;
+    opens com.soccernow.ui.soccernowui.dto.games to org.hibernate.validator;
+    opens com.soccernow.ui.soccernowui.dto to org.hibernate.validator;
+
+    opens com.soccernow.ui.soccernowui.controller to javafx.fxml;
+
     exports com.soccernow.ui.soccernowui;
+    exports com.soccernow.ui.soccernowui.util;
+    exports com.soccernow.ui.soccernowui.controller;
+    exports com.soccernow.ui.soccernowui.dto;
 }
