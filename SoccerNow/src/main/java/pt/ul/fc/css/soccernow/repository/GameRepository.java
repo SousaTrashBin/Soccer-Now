d569cd3228c7a5e510e0394f92d79757cdb27d76
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-05-28 23:26:41 +0100

    Add needed method for the filtering

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/repository/GameRepository.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/repository/GameRepository.java
@@ -1,7 +1,13 @@
 package pt.ul.fc.css.soccernow.repository;
 
 import pt.ul.fc.css.soccernow.domain.entities.game.Game;
+import pt.ul.fc.css.soccernow.util.GameSearchParams;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public interface GameRepository extends SoftDeletedRepository<Game> {
-
+    default List<Game> findAllNotDeleted(GameSearchParams params) {
+        return new ArrayList<>();
+    }
 }
--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/repository/TeamRepository.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/repository/TeamRepository.java
@@ -1,7 +1,15 @@
 package pt.ul.fc.css.soccernow.repository;
 
 import pt.ul.fc.css.soccernow.domain.entities.Team;
+import pt.ul.fc.css.soccernow.util.TeamSearchParams;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public interface TeamRepository extends SoftDeletedRepository<Team> {
     Team findByName(String name);
+
+    default List<Team> findAllNotDeleted(TeamSearchParams params) {
+        return new ArrayList<>();
+    }
 }
