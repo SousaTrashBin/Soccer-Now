4448a08cb3b0d418a240326f81e387546700bcd1
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-05-28 23:29:00 +0100

    Update to now extend the querydslexecutor (for the filter queries)

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/repository/SoftDeletedRepository.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/repository/SoftDeletedRepository.java
@@ -1,6 +1,7 @@
 package pt.ul.fc.css.soccernow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 import pt.ul.fc.css.soccernow.util.SoftDeleteEntity;
 
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @NoRepositoryBean
-public interface SoftDeletedRepository<T extends SoftDeleteEntity> extends JpaRepository<T, UUID> {
+public interface SoftDeletedRepository<T extends SoftDeleteEntity> extends JpaRepository<T, UUID>, QuerydslPredicateExecutor<T> {
     List<T> findByDeletedAtIsNull();
 
     default List<T> findAllNotDeleted() {
