68c9aefb80ba3bbcd429c4c107595de975821de5
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-06-02 15:50:13 +0100

    Add new service functions needed for the 2nd delivery of the project

--- SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/PointTournamentServiceImpl.java
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/service/impl/PointTournamentServiceImpl.java
@@ -1,39 +1,168 @@
 package pt.ul.fc.css.soccernow.service.impl;
 
+import org.springframework.stereotype.Service;
+import pt.ul.fc.css.soccernow.domain.entities.Team;
+import pt.ul.fc.css.soccernow.domain.entities.game.Game;
 import pt.ul.fc.css.soccernow.domain.entities.tournament.point.PointTournament;
+import pt.ul.fc.css.soccernow.domain.entities.tournament.point.TeamPoints;
+import pt.ul.fc.css.soccernow.exception.BadRequestException;
+import pt.ul.fc.css.soccernow.exception.ResourceDoesNotExistException;
+import pt.ul.fc.css.soccernow.repository.PointTournamentRepository;
+import pt.ul.fc.css.soccernow.service.GameService;
 import pt.ul.fc.css.soccernow.service.PointTournamentService;
+import pt.ul.fc.css.soccernow.service.TeamService;
+import pt.ul.fc.css.soccernow.util.GameStatusEnum;
+import pt.ul.fc.css.soccernow.util.TournamentSearchParams;
+import pt.ul.fc.css.soccernow.util.TournamentStatusEnum;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class PointTournamentServiceImpl implements PointTournamentService {
+    private final PointTournamentRepository pointTournamentRepository;
+    private final GameService gameService;
+    private final TeamService teamService;
+
+    public PointTournamentServiceImpl(PointTournamentRepository pointTournamentRepository, GameService gameService, TeamService teamService) {
+        this.pointTournamentRepository = pointTournamentRepository;
+        this.gameService = gameService;
+        this.teamService = teamService;
+    }
+
     @Override
     public PointTournament add(PointTournament entity) {
-        return null;
+        PointTournament pointTournament = new PointTournament();
+        pointTournament.setName(entity.getName());
+        return pointTournamentRepository.save(pointTournament);
     }
 
     @Override
     public PointTournament update(PointTournament entity) {
-        return null;
+        return null; // doesn't need to be implemented for now i think
     }
 
     @Override
     public void softDelete(UUID entityId) {
-
+        // doesn't need to be implemented for now i think
     }
 
     @Override
     public List<PointTournament> findAllNotDeleted() {
-        return List.of();
+        return pointTournamentRepository.findAllNotDeleted();
     }
 
     @Override
     public PointTournament findById(UUID entityId) {
-        return null;
+        return pointTournamentRepository.findById(entityId)
+                .orElseThrow(() -> new ResourceDoesNotExistException("Point Tournament", "id", entityId));
     }
 
     @Override
     public PointTournament findNotDeletedById(UUID entityId) {
-        return null;
+        PointTournament pointTournament = findById(entityId);
+        if (pointTournament.isDeleted()) {
+            throw new ResourceDoesNotExistException("Point Tournament", "id", pointTournament);
+        }
+        return pointTournament;
     }
+
+    @Override
+    public List<PointTournament> findAllNotDeleted(TournamentSearchParams params) {
+        return pointTournamentRepository.findAllNotDeleted(params);
+    }
+
+    @Override
+    public PointTournament addGameToTournament(UUID gameId, UUID tournamentId) {
+        PointTournament savedPointTournament = findNotDeletedById(tournamentId);
+        if (savedPointTournament.getStatus() != TournamentStatusEnum.IN_PROGRESS) {
+            throw new BadRequestException("Tournament must be in progress in order to add new games to it");
+        }
+        Game savedGame = gameService.findNotDeletedById(gameId);
+        if (savedGame.getStatus() != GameStatusEnum.OPENED) {
+            throw new BadRequestException("Game must be opened in order to be added to a tournament");
+        }
+        if (savedPointTournament.hasGame(savedGame)) {
+            throw new BadRequestException("Game is already in the tournament");
+        }
+        savedPointTournament.addGame(savedGame);
+        return pointTournamentRepository.save(savedPointTournament);
+    }
+
+    @Override
+    public PointTournament removeGameFromTournament(UUID gameId, UUID tournamentId) {
+        PointTournament savedPointTournament = findNotDeletedById(tournamentId);
+        if (savedPointTournament.getStatus() != TournamentStatusEnum.IN_PROGRESS) {
+            throw new BadRequestException("Tournament must be in progress in order to add new games to it");
+        }
+        Game savedGame = gameService.findNotDeletedById(gameId);
+        if (savedGame.getStatus() != GameStatusEnum.OPENED) {
+            throw new BadRequestException("Game must be opened in order to be added to a tournament");
+        }
+        if (!savedPointTournament.hasGame(savedGame)) {
+            throw new BadRequestException("Game is not part of the tournament");
+        }
+        savedPointTournament.removeGame(savedGame);
+        return pointTournamentRepository.save(savedPointTournament);
+    }
+
+
+    @Override
+    public PointTournament addTeamToTournament(UUID teamId, UUID tournamentId) {
+        PointTournament savedPointTournament = findNotDeletedById(tournamentId);
+        if (savedPointTournament.getStatus() != TournamentStatusEnum.OPEN) {
+            throw new BadRequestException("Tournament must be opened in order to add new teams to it");
+        }
+        Team savedTeam = teamService.findNotDeletedById(teamId);
+        if (savedPointTournament.hasTeam(savedTeam)) {
+            throw new BadRequestException("Team is already in the tournament");
+        }
+        savedPointTournament.addTeam(savedTeam);
+        return pointTournamentRepository.save(savedPointTournament);
+    }
+
+    @Override
+    public PointTournament removeTeamFromTournament(UUID teamId, UUID tournamentId) {
+        PointTournament savedPointTournament = findNotDeletedById(tournamentId);
+        if (savedPointTournament.getStatus() != TournamentStatusEnum.OPEN) {
+            throw new BadRequestException("Tournament must be opened in order to add new teams to it");
+        }
+        Team savedTeam = teamService.findNotDeletedById(teamId);
+        if (!savedPointTournament.hasTeam(savedTeam)) {
+            throw new BadRequestException("Team is not part of the tournament");
+        }
+        savedPointTournament.removeTeam(savedTeam);
+        return pointTournamentRepository.save(savedPointTournament);
+    }
+
+    @Override
+    public PointTournament closeRegistrations(UUID tournamentId) {
+        PointTournament tournament = findNotDeletedById(tournamentId);
+        if (tournament.getStatus() != TournamentStatusEnum.OPEN) {
+            throw new BadRequestException("Tournament is not open for registration");
+        }
+        if (!tournament.hasMinimumTeams()) {
+            throw new BadRequestException("Tournament must have at least " + PointTournament.MINIMUM_TEAM_SIZE + " teams in order to close registrations");
+        }
+        tournament.setStatus(TournamentStatusEnum.IN_PROGRESS);
+        return pointTournamentRepository.save(tournament);
+    }
+
+    @Override
+    public PointTournament endTournament(UUID tournamentId) {
+        PointTournament tournament = findNotDeletedById(tournamentId);
+        if (!tournament.getStatus().equals(TournamentStatusEnum.IN_PROGRESS)) {
+            throw new IllegalStateException("Tournament is not in progress.");
+        }
+        tournament.setStatus(TournamentStatusEnum.CLOSED);
+
+        List<TeamPoints> sortedTeamPoints = new ArrayList<>(tournament.getTeamPoints());
+
+        for (int i = 0; i < sortedTeamPoints.size(); i++) {
+            sortedTeamPoints.get(i).updatePlacementForTournament(tournament, i);
+        }
+        return pointTournamentRepository.save(tournament);
+    }
+
 }
