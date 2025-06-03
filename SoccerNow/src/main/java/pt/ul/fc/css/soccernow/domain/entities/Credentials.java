c88ee5177e8d88985ff4f4a0bdb9406f1cc91411
Diogo Sousa fc59792 <fc59792@alunos.fc.ul.pt>
2025-06-04 00:35:26 +0100

    Add new mock credentials related utils (just for the login)

---
+++ SoccerNow/src/main/java/pt/ul/fc/css/soccernow/domain/entities/Credentials.java
@@ -0,0 +1,45 @@
+package pt.ul.fc.css.soccernow.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "credentials")
+public class Credentials {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+}
