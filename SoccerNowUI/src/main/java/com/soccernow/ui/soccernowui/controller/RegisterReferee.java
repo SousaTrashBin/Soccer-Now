b3463b3dec770437716d6d8e3cc16f28cb30901f
Sofia <sofia.reia.pt@gmail.com>
2025-06-04 00:42:15 +0100

    Add register referee

---
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/RegisterReferee.java
@@ -0,0 +1,35 @@
+package com.soccernow.ui.soccernowui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.util.Objects;
+
+public class RegisterReferee {
+
+    @FXML
+    private ComboBox<String> hasCertificateComboBox;
+
+    @FXML
+    public void initialize() {
+        hasCertificateComboBox.getItems().add(null);
+        hasCertificateComboBox.getItems().addAll("Yes", "No");
+        hasCertificateComboBox.setValue("No");
+    }
+
+    public boolean getCertificateBoolean() {
+        String selected = hasCertificateComboBox.getValue();
+        return Objects.equals(selected, "Yes");
+    }
+
+    @FXML
+    public void onRegisterClick(ActionEvent actionEvent) {
+        return;
+    }
+
+    @FXML
+    public void onBackClick(ActionEvent actionEvent) {
+        return;
+    }
+}
