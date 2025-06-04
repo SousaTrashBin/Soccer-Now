1cd39c4fa92534b5c0a1f7a99c6b4759f8004006
Sofia <sofia.reia.pt@gmail.com>
2025-06-04 03:40:55 +0100

    Update register team controller

--- SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/team/RegisterTeam.java
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/team/RegisterTeam.java
@@ -1,4 +1,35 @@
 package com.soccernow.ui.soccernowui.controller.team;
 
+import com.soccernow.ui.soccernowui.util.FXMLUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+
 public class RegisterTeam {
+
+    @FXML
+    public void onAddPlayerClick(ActionEvent actionEvent) {
+        return;
+    }
+
+    @FXML
+    public void onRemovePlayerClick(ActionEvent actionEvent) {
+        return;
+    }
+
+    @FXML
+    public void onCancelClick(ActionEvent actionEvent) {
+        return;
+    }
+
+    @FXML
+    public void onRegisterClick(ActionEvent actionEvent) {
+        return;
+    }
+
+    @FXML
+    public void onBackClick(ActionEvent actionEvent) {
+        FXMLUtils.switchScene("/com/soccernow/ui/soccernowui/fxml/home/home-screen.fxml",
+                (Node) actionEvent.getSource());
+    }
 }
