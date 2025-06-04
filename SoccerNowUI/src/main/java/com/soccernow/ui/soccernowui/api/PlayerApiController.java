60591aa3b58939de7e95ac2c174da60b6bd09ccc
Sousa <sousanamain@gmail.com>
2025-06-04 16:33:32 +0000

    Add Api Controller skeletons

--- SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/api/PlayerApiController.java
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/api/PlayerApiController.java
@@ -2,9 +2,11 @@ package com.soccernow.ui.soccernowui.api;
 
 import com.soccernow.ui.soccernowui.dto.user.PlayerDTO;
 
+import static com.soccernow.ui.soccernowui.util.ApiUtils.baseURL;
+
 public enum PlayerApiController {
     INSTANCE;
-    private String url = "/players/";
+    private String playersURL = baseURL + "players/";
 
     public PlayerDTO registerPlayer(PlayerDTO player) {
         return new PlayerDTO();
