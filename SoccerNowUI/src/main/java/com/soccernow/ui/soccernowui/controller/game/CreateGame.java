b49636e03c3a97cee80d3ad7c29fb4902d987a52
Sofia <sofia.reia.pt@gmail.com>
2025-06-04 03:25:35 +0100

    Update create game component + controller

--- SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/game/CreateGame.java
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/game/CreateGame.java
@@ -1,4 +1,21 @@
 package com.soccernow.ui.soccernowui.controller.game;
 
+import com.soccernow.ui.soccernowui.util.FXMLUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+
+
 public class CreateGame {
+
+    @FXML
+    public void onCreateGameClick(ActionEvent event) {
+        return;
+    }
+
+    @FXML
+    public void onBackClick(ActionEvent actionEvent) {
+        FXMLUtils.switchScene("/com/soccernow/ui/soccernowui/fxml/home/home-screen.fxml",
+                (Node) actionEvent.getSource());
+    }
 }
