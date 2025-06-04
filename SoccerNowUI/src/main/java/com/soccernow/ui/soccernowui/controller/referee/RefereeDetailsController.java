3b1ceac82cf7920639ab9454faa9c8334e3aa2a6
Sofia <sofia.reia.pt@gmail.com>
2025-06-05 00:09:50 +0100

    Update fix to referee details controller

--- SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/referee/RefereeDetailsController.java
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/referee/RefereeDetailsController.java
@@ -1,4 +1,30 @@
 package com.soccernow.ui.soccernowui.controller.referee;
 
+import com.soccernow.ui.soccernowui.util.FXMLUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class RefereeDetailsController {
+
+    @FXML
+    private TextField refereeNameField;
+
+    @FXML
+    private ComboBox<String> certificateComboBox;
+
+    @FXML
+    public void initialize() {
+    }
+
+    @FXML
+    private void onSaveClick() {
+    }
+
+    public void onBackClick(ActionEvent actionEvent) {
+        FXMLUtils.switchScene("/com/soccernow/ui/soccernowui/fxml/player/player-list.fxml",
+                (Node) actionEvent.getSource());
+    }
 }
