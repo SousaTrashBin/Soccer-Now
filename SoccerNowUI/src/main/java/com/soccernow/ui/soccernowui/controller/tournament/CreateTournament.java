7581788adcb76394501b15cd8a9ee492a2d9a5a5
Sofia <sofia.reia.pt@gmail.com>
2025-06-04 03:46:39 +0100

    Update create tournament

--- SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/tournament/CreateTournament.java
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/tournament/CreateTournament.java
@@ -1,4 +1,35 @@
 package com.soccernow.ui.soccernowui.controller.tournament;
 
+import com.soccernow.ui.soccernowui.util.FXMLUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+
 public class CreateTournament {
+
+    @FXML
+    public void onAddGameClick(ActionEvent event) {
+        return;
+    }
+
+    @FXML
+    public void onRemoveGameClick(ActionEvent event) {
+        return;
+    }
+
+    @FXML
+    public void onCancelClick(ActionEvent event) {
+        return;
+    }
+
+    @FXML
+    public void onCreateClick(ActionEvent event) {
+        return;
+    }
+
+    @FXML
+    public void onBackClick(ActionEvent actionEvent) {
+        FXMLUtils.switchScene("/com/soccernow/ui/soccernowui/fxml/home/home-screen.fxml",
+                (Node) actionEvent.getSource());
+    }
 }
