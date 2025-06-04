94488ceb94a0dd5df950182f7c5d18f1c91a3df9
Sofia <sofia.reia.pt@gmail.com>
2025-06-05 00:00:07 +0100

    Update create game and game result controller skeleton

--- SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/game/RegisterGameResultsController.java
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/game/RegisterGameResultsController.java
@@ -1,4 +1,40 @@
 package com.soccernow.ui.soccernowui.controller.game;
 
+import com.soccernow.ui.soccernowui.dto.user.PlayerInfoDTO;
+import com.soccernow.ui.soccernowui.dto.user.RefereeInfoDTO;
+import com.soccernow.ui.soccernowui.util.CardEnum;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class RegisterGameResultsController {
+
+    @FXML
+    private TextField teamOneGoalsField;
+    @FXML
+    private TextField teamTwoGoalsField;
+    @FXML
+    private ComboBox<String> teamComboBox;
+    @FXML
+    private ComboBox<PlayerInfoDTO> playerComboBox;
+    @FXML
+    private TextField playerGoalsField;
+    @FXML
+    private ComboBox<CardEnum> cardTypeComboBox;
+    @FXML
+    private ComboBox<RefereeInfoDTO> refereeComboBox;
+    @FXML
+    private ListView<String> playerCardsListView;
+
+    @FXML
+    public void initialize() {
+    }
+
+    @FXML
+    private void onSavePlayerStatsClick() {
+    }
+
+    @FXML
+    private void onSaveGameClick() {
+    }
+
 }
