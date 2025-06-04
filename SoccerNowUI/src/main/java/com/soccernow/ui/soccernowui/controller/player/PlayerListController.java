96955c455d0a3e21a94b8a82047e7f74363834ed
Sousa <sousanamain@gmail.com>
2025-06-04 20:33:04 +0000

    Fix to initialize player list

--- SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/player/PlayerListController.java
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/controller/player/PlayerListController.java
@@ -1,10 +1,23 @@
 package com.soccernow.ui.soccernowui.controller.player;
 
+import com.soccernow.ui.soccernowui.api.PlayerApiController;
+import com.soccernow.ui.soccernowui.dto.user.PlayerDTO;
+import com.soccernow.ui.soccernowui.util.ErrorException;
 import com.soccernow.ui.soccernowui.util.FXMLUtils;
+import com.soccernow.ui.soccernowui.util.FutsalPositionEnum;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PlayerListController {
@@ -13,9 +26,37 @@ public class PlayerListController {
     private VBox createPlayerVBox;
 
     @FXML
+    private TableColumn<PlayerDTO, String> nameColumn;
+
+    @FXML
+    private TableColumn<PlayerDTO, String> positionColumn;
+
+    @FXML
+    private TableView<PlayerDTO> playerTableView;
+
+    public void initialize() {
+        nameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+
+        positionColumn.setCellValueFactory(cellData -> {
+            FutsalPositionEnum pos = cellData.getValue().getPreferredPosition();
+            String posName = pos != null ? pos.name() : "None";
+            return new SimpleStringProperty(posName);
+        });
+
+        List<PlayerDTO> players;
+        try {
+            players = PlayerApiController.INSTANCE.getAllPlayers();
+        } catch (IOException | ErrorException e) {
+            players = new ArrayList<>();
+        }
+        ObservableList<PlayerDTO> playersObservable = FXCollections.observableArrayList(players);
+        playerTableView.setItems(playersObservable);
+    }
+
+    @FXML
     private void onAddClick(ActionEvent event) {
         FXMLUtils.switchScene(
-                "/com/soccernow/ui/soccernowui/fxml/game/register-player.fxml",
+                "/com/soccernow/ui/soccernowui/fxml/player/register-player.fxml",
                 (Node) event.getSource()
         );
     }
