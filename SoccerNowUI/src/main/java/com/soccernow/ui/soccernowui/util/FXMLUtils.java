0add8df444910d346b6706c882fe1bd2188a2d20
Sousa <sousanamain@gmail.com>
2025-06-04 18:54:46 +0000

    Add new validation util for the DTO's

--- SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/util/FXMLUtils.java
+++ SoccerNowUI/src/main/java/com/soccernow/ui/soccernowui/util/FXMLUtils.java
@@ -1,12 +1,17 @@
 package com.soccernow.ui.soccernowui.util;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.function.Function;
 
 public class FXMLUtils {
 
@@ -21,5 +26,24 @@ public class FXMLUtils {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static <T> boolean validateAndShowAlert(T objectToValidate, Validator validator) {
+        Set<ConstraintViolation<T>> violations = validator.validate(objectToValidate);
+
+        if (!violations.isEmpty()) {
+            StringBuilder errorMessage = new StringBuilder("Please correct the following errors:\n");
+            for (ConstraintViolation<T> violation : violations) {
+                errorMessage.append("- ").append(violation.getMessage()).append("\n");
+            }
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Validation Error");
+            alert.setHeaderText("Invalid Input");
+            alert.setContentText(errorMessage.toString());
+            alert.showAndWait();
+            return false;
+        }
+        return true;
     }
 }
